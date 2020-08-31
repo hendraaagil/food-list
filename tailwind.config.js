68534d0a8cdf5ea14d1c680580a46a778c3a1e28
hendra-agil <agilsyaputra26@gmail.com>
2020-08-31 18:29:55 +0700

    Tailwind Config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '#888883',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
